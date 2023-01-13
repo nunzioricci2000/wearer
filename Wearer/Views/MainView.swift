866c974fc388adb53a335a552b0867e7339eb9d6
spicyhotnoodles <pietro.ciuci@gmail.com>
2023-01-13 09:31:43 +0100

    Aggiunta finta modale

--- Wearer/Views/MainView.swift
+++ Wearer/Views/MainView.swift
@@ -11,6 +11,31 @@ struct MainView: View {
     var body: some View {
         ZStack {
             TopWeather()
+            BottomSheetView(isOpen: .constant(true), maxHeight: 480) {
+                //Rectangle().fill(Color.red)
+                //.edgesIgnoringSafeArea(.all)
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Today's Suggestions")
+                        .font(.system(size: 28, weight: .semibold, design: .rounded))
+                    HStack(spacing: 20) {
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color(.systemGray3))
+                            .frame(width: 170, height: 170)
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color(.systemGray3))
+                            .frame(width: 170, height: 170)
+                    }
+                    HStack(spacing: 20) {
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color(.systemGray3))
+                            .frame(width: 170, height: 170)
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color(.systemGray3))
+                            .frame(width: 170, height: 170)
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
         }
     }
 }
