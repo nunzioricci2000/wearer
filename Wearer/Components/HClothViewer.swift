f5dace8262ffff2f473e5811aa193ca6dcf47af9
Hendrix88 <amura22@fed.idserve.net>
2023-01-16 12:50:58 +0100

    Wardrobe View
    Creazione Modale

--- Wearer/Components/HClothViewer.swift
+++ Wearer/Components/HClothViewer.swift
@@ -8,53 +8,63 @@
 import SwiftUI
 
 struct HClothViewer: View {
+    @State var isPresented: Bool = false
     var cloth : Cloth
     var body: some View {
         VStack{
-            HStack {
-                Text(cloth.type.rawValue)
-                    .fontWeight(.heavy)
-                    .font(.title)
-                Image(systemName: "chevron.down")
-                    .padding()
-                Spacer()
+            Button {
+                isPresented = true
             }
-            .padding(.leading, 20.0)
-                ScrollView(.horizontal, showsIndicators: false){
-                    HStack{
-                        Image("Jeans1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 165, height: 165)
-                            .clipShape(Rectangle())
-                            .cornerRadius(30)
-                        Image("Jeans1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 165, height: 165)
-                            .clipShape(Rectangle())
-                            .cornerRadius(30)
-                        Image("Jeans1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 165, height: 165)
-                            .clipShape(Rectangle())
-                            .cornerRadius(30)
-                        Image("Jeans1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 165, height: 165)
-                            .clipShape(Rectangle())
-                            .cornerRadius(30)
-                        Image("Jeans1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 165, height: 165)
-                            .clipShape(Rectangle())
-                            .cornerRadius(30)
+            label: {
+                HStack {
+                    Text(cloth.type.rawValue)
+                        .fontWeight(.heavy)
+                        .font(.title)
+                    Image(systemName: "chevron.down")
+                        .padding()
+                    Spacer()
                 }
-                    .padding(.leading)
+                .padding(.leading, 20.0)
             }
+            .buttonStyle(PlainButtonStyle())
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack{
+                    Image("Jeans1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 165, height: 165)
+                        .clipShape(Rectangle())
+                        .cornerRadius(30)
+                    Image("Jeans1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 165, height: 165)
+                        .clipShape(Rectangle())
+                        .cornerRadius(30)
+                    Image("Jeans1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 165, height: 165)
+                        .clipShape(Rectangle())
+                        .cornerRadius(30)
+                    Image("Jeans1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 165, height: 165)
+                        .clipShape(Rectangle())
+                        .cornerRadius(30)
+                    Image("Jeans1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 165, height: 165)
+                        .clipShape(Rectangle())
+                        .cornerRadius(30)
+                }
+                .padding(.leading)
+            }
+        }
+        .sheet(isPresented: $isPresented) {
+            ClothesList()
         }
     }
 }
---
+++ Wearer/Views/ClothesList.swift
@@ -0,0 +1,27 @@
+//
+//  ClothesList.swift
+//  Wearer
+//
+//  Created by Alberto Mura on 16/01/23.
+//
+
+import SwiftUI
+
+struct ClothesList: View {
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        let cloth = Cloth(name: "Sabaku", type: ClothType.jacket, picture: "")
+        let jeans = Cloth(name: "Jeans Figo", type: ClothType.jeans, picture: "")
+        let shoes = Cloth(name: "Scarpe Fighe", type: ClothType.shoes, picture: "")
+        let shirt = Cloth(name: "Scarpe Fighe", type: ClothType.shirt, picture: "")
+        ScrollView(showsIndicators: false) {
+            HClothViewer(cloth: cloth)
+        }
+    }
+}
+struct ClothesList_Previews: PreviewProvider {
+    static var previews: some View {
+        ClothesList()
+    }
+}
