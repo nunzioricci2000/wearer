295189602390101c272ae3bfd1c1a9fa7055d89c
Desyy22 <113532706+Desyy22@users.noreply.github.com>
2023-01-19 10:54:42 +0100

    Added struct to convert weather codes from api to it's relative description and icon

--- Wearer/Model/OMResponse.swift
+++ Wearer/Model/OMResponse.swift
@@ -15,15 +15,46 @@ struct OMHourly: Decodable {
 }
 
 /*struct OMHourlyUnits: Decodable {
-    var time: String
-    var temperature_2m: String
-}*/
-
+ var time: String
+ var temperature_2m: String
+ }*/
+// Converting the weather code in the relative description and weather icon
 struct OMForeground: Decodable {
-    var time: String
-    var temperature: Double
+    var weatherDescription: String
+    var weatherIcon: String
+    
+    static func from(code: Int) -> Self {
+        let foreground: Self
+        switch code {
+        case 0: foreground = .init(weatherDescription: "Clear sky",
+                                   weatherIcon: "giorno.sereno")
+        case 1, 2, 3: foreground = .init(weatherDescription: "Partly Cloudy",
+                                         weatherIcon: "giorno.pnuvoloso")
+        case 45, 48: foreground = .init(weatherDescription: "Fog",
+                                        weatherIcon: "nebbia")
+        case 51, 53, 55:foreground = .init(weatherDescription: "Drizzle",
+                                           weatherIcon: "pioggia.debole")
+        case 56, 57, 66, 67: foreground = .init(weatherDescription: "Hail",
+                                                weatherIcon: "grandine")
+        case 61, 63, 65: foreground = .init(weatherDescription: "Rain",
+                                            weatherIcon: "pioggia")
+        case 71, 73, 75, 77, 85, 86: foreground = .init(weatherDescription: "Snow",
+                                                        weatherIcon: "neve")
+        case 80, 81, 82: foreground = .init(weatherDescription: "Heavy Rain",
+                                            weatherIcon: "pioggia.forte")
+        case 95, 96, 99: foreground = .init(weatherDescription: "Thunderstorm",
+                                            weatherIcon: "tempesta")
+            
+            
+            
+            
+            
+        default:
+            fatalError("Codice non trovato")
+        }
+        return foreground
+    }
 }
-
 struct OMCurrentWeather: Decodable {
     var temperature: Double
     var weatherCode: Double
@@ -46,14 +77,14 @@ struct OMResponse: Decodable {
 }
 
 /*var foregrounds: [OMForeground] {
-    var foregrounds = [OMForeground]()
-    for i in 0..<foregrounds.count {
-        foregrounds.append(.init(time: hourly.time[i], temperature: hourly.temperature_2m[i]))
-    }
-    return foregrounds
-}*/
+ var foregrounds = [OMForeground]()
+ for i in 0..<foregrounds.count {
+ foregrounds.append(.init(time: hourly.time[i], temperature: hourly.temperature_2m[i]))
+ }
+ return foregrounds
+ }*/
 
 /*struct Position: Decodable {
-    var latitude: Double
-    var longitude: Double
-}*/
+ var latitude: Double
+ var longitude: Double
+ }*/
