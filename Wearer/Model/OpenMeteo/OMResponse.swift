f73a6250d8b35dbd3d61f804d1be8895b1a05dee
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-01-23 10:07:22 +0100

    Update OMResponse.swift
    Added custom init

--- Wearer/Model/OpenMeteo/OMResponse.swift
+++ Wearer/Model/OpenMeteo/OMResponse.swift
@@ -47,13 +47,21 @@ struct OMResponse: Decodable {
         case hourly
     }
     struct Hourly: Decodable {
-        var time: [String]
+        var time: [Date]
         var temperature: [Double]
         var weatherCode: [Int]
         enum CodingKeys: String, CodingKey {
             case time
             case temperature = "temperature_2m"
             case weatherCode = "weathercode"
+        }
+        init(from decoder: Decoder) throws {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm"
+            let container: KeyedDecodingContainer<OMResponse.Hourly.CodingKeys> = try decoder.container(keyedBy: OMResponse.Hourly.CodingKeys.self)
+            self.time = try container.decode([String].self, forKey: OMResponse.Hourly.CodingKeys.time).map({ dateFormatter.date(from: $0)! })
+            self.temperature = try container.decode([Double].self, forKey: OMResponse.Hourly.CodingKeys.temperature)
+            self.weatherCode = try container.decode([Int].self, forKey: OMResponse.Hourly.CodingKeys.weatherCode)
         }
     }
     struct CurrentWeather: Decodable {
