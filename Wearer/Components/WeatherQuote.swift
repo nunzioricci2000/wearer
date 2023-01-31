aa047d56ccc98497e23175cf39687715bfd84d1b
Hendrix88 <amura22@fed.idserve.net>
2023-01-31 10:56:59 +0100

    WeatherQuote Component
    Weather quote component creation

---
+++ Wearer/Components/WeatherQuote.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherQuote.swift
+//  Wearer
+//
+//  Created by Alberto Mura on 31/01/23.
+//
+
+import SwiftUI
+
+
+struct WeatherQuote: View {
+    @State var text: String
+    var body: some View {
+        Text(text)
+            .foregroundColor(.white)
+            .fontWeight(.light)
+    }
+}
+
+struct WeatherQuote_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherQuote(text: "Seems like it could rain today, don't forget the umbrella")
+            .background(.blue)
+    }
+}
