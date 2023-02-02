5ba054557938f22b971f792610dfb576032ab60e
spicyhotnoodles <pietro.ciuci@gmail.com>
2023-02-02 09:38:15 +0100

    Dismantled HCloth and GridCloth views

--- Wearer/Components/GridClothViewer.swift
+++ Wearer/Components/GridClothViewer.swift
@@ -1,55 +1,55 @@
+////
+////  GridClothViewer.swift
+////  Wearer
+////
+////  Created by Alberto Mura on 17/01/23.
+////
 //
-//  GridClothViewer.swift
-//  Wearer
+//import SwiftUI
 //
-//  Created by Alberto Mura on 17/01/23.
-//
-
-import SwiftUI
-
-struct GridClothViewer: View {
-    @State var isPresented: Bool = false
-    @State var isSelected: Bool = false
-    @State var clothType: String
-    @FetchRequest(entity: Cloth.entity(), sortDescriptors: []) var clothes: FetchedResults<Cloth>
-    var body: some View {
-        VStack {
-            Text("Jackets")
-                .bold()
-                .font(.title2)
-            ScrollView(.vertical) {
-                LazyVGrid(columns: [GridItem(alignment: .trailing), GridItem(alignment: .leading)]) {
-                    ForEach(clothes) { cloth in
-                        if let data = cloth.picture ?? UIImage(named: "ClothPlaceholder")!.pngData(),
-                           let image = UIImage(data: data) ?? UIImage(named: "ClothPlaceholder")!,
-                           clothType.lowercased() == cloth.type?.lowercased() {
-                            Image(uiImage: image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 165, height: 165)
-                                .clipShape(Rectangle())
-                                .cornerRadius(30)
-                        }
-                    }
-                    Button {
-                        isPresented = true
-                        isSelected.toggle()
-                    }
-                label: {
-                    HStack {
-                        Image("NewCloth")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 165, height: 165)
-                            .clipShape(Rectangle())
-                        .cornerRadius(30)                        }
-                }
-                }
-            }
-        }
-        .padding(.top, 8.0)
-        .sheet(isPresented: $isPresented) {
-            ClothCreationView(name: "")
-        }
-    }
-}
+//struct GridClothViewer: View {
+//    @State var isPresented: Bool = false
+//    @State var isSelected: Bool = false
+//    @State var clothType: String
+//    @FetchRequest(entity: Cloth.entity(), sortDescriptors: []) var clothes: FetchedResults<Cloth>
+//    var body: some View {
+//        VStack {
+//            Text("Jackets")
+//                .bold()
+//                .font(.title2)
+//            ScrollView(.vertical) {
+//                LazyVGrid(columns: [GridItem(alignment: .trailing), GridItem(alignment: .leading)]) {
+//                    ForEach(clothes) { cloth in
+//                        if let data = cloth.picture ?? UIImage(named: "ClothPlaceholder")!.pngData(),
+//                           let image = UIImage(data: data) ?? UIImage(named: "ClothPlaceholder")!,
+//                           clothType.lowercased() == cloth.type?.lowercased() {
+//                            Image(uiImage: image)
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fill)
+//                                .frame(width: 165, height: 165)
+//                                .clipShape(Rectangle())
+//                                .cornerRadius(30)
+//                        }
+//                    }
+//                    Button {
+//                        isPresented = true
+//                        isSelected.toggle()
+//                    }
+//                label: {
+//                    HStack {
+//                        Image("NewCloth")
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fill)
+//                            .frame(width: 165, height: 165)
+//                            .clipShape(Rectangle())
+//                        .cornerRadius(30)                        }
+//                }
+//                }
+//            }
+//        }
+//        .padding(.top, 8.0)
+//        .sheet(isPresented: $isPresented) {
+//            ClothCreationView(name: "")
+//        }
+//    }
+//}
--- Wearer/Components/HClothViewer.swift
+++ Wearer/Components/HClothViewer.swift
@@ -1,57 +1,57 @@
+////
+////  ShoesView.swift
+////  Wearer
+////
+////  Created by Alberto Mura on 13/01/23.
+////
 //
-//  ShoesView.swift
-//  Wearer
+//import SwiftUI
 //
-//  Created by Alberto Mura on 13/01/23.
+// swiftlint:disable all
 //
-
-import SwiftUI
-
-// swiftlint: disable all
-
-struct HClothViewer: View {
-    let generator = UIImpactFeedbackGenerator(style: .light)
-    @FetchRequest(entity: Cloth.entity(), sortDescriptors: []) var clothes: FetchedResults<Cloth>
-    @State var clothType: String = "Panths"
-    var body: some View {
-        VStack {
-            NavigationLink {
-                GridClothViewer(clothType: clothType)
-            } label: {
-                HStack {
-                    Text(clothType)
-                        .fontWeight(.heavy)
-                        .font(.title)
-                    Image(systemName: "plus.circle.fill")
-                    Spacer()
-                }
-                .padding(.leading, 20.0)
-            }
-            .buttonStyle(PlainButtonStyle())
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(clothes) { cloth in
-                        if clothType.lowercased() == cloth.type?.lowercased() {
-                            NavigationLink(destination: {
-                                ClothEditorView(cloth: cloth, image: UIImage(data: cloth.picture ?? UIImage(imageLiteralResourceName: "ClothPlaceholder").pngData()!), type: cloth.type ?? "Unknown", warmIndex: Int(cloth.warmness))
-                            }, label: {
-                                Image(uiImage: .init(data: cloth.picture ?? UIImage(imageLiteralResourceName: "ClothPlaceholder").pngData()!) ?? UIImage(imageLiteralResourceName: "ClothPlaceholder"))
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 165, height: 165)
-                                    .clipShape(Rectangle())
-                                .cornerRadius(30)
-                            })
-                        }
-                    }
-                }.padding(.leading)
-            }
-        }
-    }
-}
-
-struct HClothViewer_Previews: PreviewProvider {
-    static var previews: some View {
-        HClothViewer()
-    }
-}
+//struct HClothViewer: View {
+//    let generator = UIImpactFeedbackGenerator(style: .light)
+//    @FetchRequest(entity: Cloth.entity(), sortDescriptors: []) var clothes: FetchedResults<Cloth>
+//    @State var clothType: String = "Panths"
+//    var body: some View {
+//        VStack {
+//            NavigationLink {
+//                GridClothViewer(clothType: clothType)
+//            } label: {
+//                HStack {
+//                    Text(clothType)
+//                        .fontWeight(.heavy)
+//                        .font(.title)
+//                    Image(systemName: "plus.circle.fill")
+//                    Spacer()
+//                }
+//                .padding(.leading, 20.0)
+//            }
+//            .buttonStyle(PlainButtonStyle())
+//            ScrollView(.horizontal, showsIndicators: false) {
+//                HStack {
+//                    ForEach(clothes) { cloth in
+//                        if clothType.lowercased() == cloth.type?.lowercased() {
+//                            NavigationLink(destination: {
+//                                ClothEditorView(cloth: cloth, image: UIImage(data: cloth.picture ?? UIImage(imageLiteralResourceName: "ClothPlaceholder").pngData()!), type: cloth.type ?? "Unknown", warmIndex: Int(cloth.warmness))
+//                            }, label: {
+//                                Image(uiImage: .init(data: cloth.picture ?? UIImage(imageLiteralResourceName: "ClothPlaceholder").pngData()!) ?? UIImage(imageLiteralResourceName: "ClothPlaceholder"))
+//                                    .resizable()
+//                                    .aspectRatio(contentMode: .fill)
+//                                    .frame(width: 165, height: 165)
+//                                    .clipShape(Rectangle())
+//                                .cornerRadius(30)
+//                            })
+//                        }
+//                    }
+//                }.padding(.leading)
+//            }
+//        }
+//    }
+//}
+//
+//struct HClothViewer_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HClothViewer()
+//    }
+//}
